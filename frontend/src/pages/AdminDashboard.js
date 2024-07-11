cc4889a50e41b8a1b626c0b1c03e99b8edb445a5
MRehman365 <rehmanarshad365@gmail.com>
2024-07-11 18:37:44 +0500

    dashborad and more

--- frontend/src/pages/AdminDashboard.js
+++ frontend/src/pages/AdminDashboard.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import AddHighlightPoint from "../components/AddHighlightPoint";
 import AddLocationAdvantagePoint from "../components/AddLocationAdvantagePoint";
 import AddQuestion from "../components/AddQuestion";
@@ -7,15 +8,33 @@ import Dashboard from "./Dashboard";
 
 const AddPoint = () => {
   const [currentView, setCurrentView] = useState("dashboard"); // Set initial state to "dashboard"
+  const [sidebarOpen, setSidebarOpen] = useState(false); // State to manage sidebar visibility
 
   const handleButtonClick = (view) => {
     setCurrentView(view);
+    setSidebarOpen(false); // Close the sidebar when a view is selected
+  };
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
   };
 
   return (
     <div className="flex bg-gray-800 h-screen overflow-hidden">
+      {/* Menu button for mobile view */}
+      <button
+        className="block lg:hidden p-4 text-white absolute top-0 right-0 z-20"
+        onClick={toggleSidebar}
+      >
+        {sidebarOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+      </button>
+
       {/* Left side: Buttons */}
-      <div className="w-1/4 bg-[#283046] p-4 shadow-2xl">
+      <div
+        className={`lg:w-1/4 bg-[#283046] p-4 shadow-2xl fixed lg:static top-0 left-0 h-full z-10 transform ${
+          sidebarOpen ? "translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 ease-in-out lg:translate-x-0`}
+      >
         <button
           className="w-full mb-4 py-2 hover:bg-gray-500 px-5 text-start text-white rounded-md"
           onClick={() => handleButtonClick("dashboard")}
@@ -49,7 +68,7 @@ const AddPoint = () => {
       </div>
 
       {/* Right side: Input fields and component based on button click */}
-      <div className="w-3/4 overflow-y-auto">
+      <div className="w-full lg:w-3/4 overflow-y-auto ml-auto">
         {currentView === "dashboard" && <Dashboard />}
         {currentView === "highlight" && <AddHighlightPoint />}
         {currentView === "locationAdvantage" && <AddLocationAdvantagePoint />}
